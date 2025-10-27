81f9e84429de52e8064d981844e28843087d490e
mgmcr <mgm.cuber@gmail.com>
2025-10-27 13:28:43 +0300

    first commit

---
+++ api/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      backend api
+    </div>
+  )
+}
+
+export default page
---
+++ front/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      frontpage
+    </div>
+  )
+}
+
+export default page
